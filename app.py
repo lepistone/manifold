70d5a6c93ce97d3b4050a54aaa17f949877fe9cc
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2015-11-25 20:37:29 +0100

    show containers, configured by env

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from docker import Client
+from docker.utils import kwargs_from_env
+
+
 app = Flask(__name__)
+app.debug = True
+doc = Client(**kwargs_from_env())
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return str(doc.containers())
 
 if __name__ == '__main__':
     app.run()
